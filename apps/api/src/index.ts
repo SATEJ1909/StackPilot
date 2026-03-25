56da8cff24cb790fde3399a99505687873877c0e
Satej G.  Niswade <satejniswade6@gmail.com>
2026-03-25 12:15:17 +0530

    fixed db connections

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello World")
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+async function main(){
+    await mongoose.connect(process.env.MONGO_URI as string)
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+main();
